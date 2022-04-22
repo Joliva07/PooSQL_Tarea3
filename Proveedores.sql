30b4e819ae4df49764ff26a91c1d0fc6bc7d9628
Josué Oliva <78836905+Joliva07@users.noreply.github.com>
2022-04-21 21:12:46 -0600

    Add files via upload

---
+++ Proveedores.sql
@@ -0,0 +1,6 @@
+SELECT `proveedores`.`idProveedores`,
+    `proveedores`.`proveedor`,
+    `proveedores`.`nit`,
+    `proveedores`.`direccion`,
+    `proveedores`.`telefono`
+FROM `db_productos`.`proveedores`;
